eac180c4d805f143030960cf16937be4da7145f8
FerDeerZzKir <121364308+FerDeerZzKir@users.noreply.github.com>
2023-12-17 17:00:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 function captureAndSave() {
-    const video = document.getElementById('video');
-    const canvas = document.getElementById('canvas');
-
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    // In this part, you can add the code for sending the image to the server and then to Telegram
-    // For example, using the Fetch API to send it to the server
-
-    // fetch('/upload', {
-    //     method: 'POST',
-    //     body: canvas.toDataURL('image/png'),
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded',
-    //     },
-    // })
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.error('Error uploading image:', error));
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('video');
+    const video = document.createElement('video');
     
     navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
             video.srcObject = stream;
+
+            video.onloadedmetadata = () => {
+                const canvas = document.createElement('canvas');
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+
+                const context = canvas.getContext('2d');
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                // Создание ссылки для скачивания
+                const link = document.createElement('a');
+                link.href = canvas.toDataURL('image/jpeg'); // Меняйте 'image/jpeg' на 'image/png', если нужно сохранять в PNG
+                link.download = 'snapshot.jpg'; // Меняйте 'snapshot.jpg' на нужное имя файла
+
+                // Эмуляция клика по ссылке для скачивания
+                link.click();
+
+                // Очистка за собой
+                document.body.removeChild(video);
+                document.body.removeChild(canvas);
+            };
         })
         .catch((error) => {
             console.error('Error accessing webcam:', error);
         });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Можете также добавить другую инициализацию здесь, если необходимо
 });
